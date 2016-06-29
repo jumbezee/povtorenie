35b416936f1437b00a435553752b520f21b4ec07
Николай Кузьменко < >
2016-06-29 21:15:43 +0300

    read content variable from post request

--- app.rb
+++ app.rb
@@ -10,3 +10,9 @@ end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+post '/new' do
+	content = params[:content]
+
+	erb "You typed #{content}"
+end
