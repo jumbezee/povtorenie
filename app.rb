d3a589cf550a1e9d9bcdc315f7d9bb59c4bc831f
Николай Кузьменко < >
2016-06-29 22:57:32 +0300

    Display comments for post

--- app.rb
+++ app.rb
@@ -58,6 +58,8 @@ get '/details/:post_id' do
 	results = @db.execute 'select * from Posts where id = ?',[post_id]
 	@row = results[0]
 
+	@comments = @db.execute 'select * from Comments where post_id=? order by id',[post_id]
+	
 	erb :details
 end
 
@@ -69,5 +71,5 @@ post '/details/:post_id' do
 				 values (?, datetime(),?)', [content, post_id]
 	
 
-	redirect to '/details/' + post_id
+	redirect to('/details/' + post_id)
 end
