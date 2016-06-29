460fac4f3d93fb5d24a7f86c33466a4e0b96ce0e
Николай Кузьменко < >
2016-06-29 22:43:09 +0300

    create Comments table

--- app.rb
+++ app.rb
@@ -21,6 +21,13 @@ configure do
 		created_date DATE,
 		content TEXT
 	)'
+	@db.execute 'CREATE TABLE IF NOT EXISTS Comments
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT,
+		post_id INTEGER
+	)'
 end
 
 get '/new' do
