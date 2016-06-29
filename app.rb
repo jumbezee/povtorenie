8359d1e0a3211ac1dab29f8132dbc7c2a04fbff9
Николай Кузьменко < >
2016-06-29 22:01:54 +0300

    add index.erb

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ get '/new' do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index 
 end
 
 post '/new' do
